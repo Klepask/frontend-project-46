012839861af44fedc00956abe87ee9930951de9c
Eva Ulnitskaya <evaulnitsakaya@yandex.ru>
2023-11-28 19:43:32 +0300

    update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,10 @@
 import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
+import { dirname, join } from 'path';
+import { readFileSync } from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
+console.log(__filename);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => join(__dirname, '.', '__fixtures__', filename);
