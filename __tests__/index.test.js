d504a1262eb3544007d1dfbd5aa6a39cddaeb185
Eva Ulnitskaya <evaulnitsakaya@yandex.ru>
2023-12-03 18:59:49 +0300

    fix tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,12 +1,12 @@
 import { fileURLToPath } from 'url';
-import path from 'path';
+import path, { dirname } from 'path';
 import fs from 'fs';
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename)
 
-const getFixturePath = (filename) => join(__dirname, '.', '__fixtures__', filename);
+const getFixturePath = (filename) => path.join(__dirname, '.', '__fixtures__', filename);
 const readFixture = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test.each([
