3311626f38e730796302280abb0c584574e0601d
Eva Ulnitskaya <evaulnitsakaya@yandex.ru>
2024-01-05 20:56:21 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
-import fs from 'fs';
-import path from 'path';
-import getParsedData from './parsers.js';
-import compare from './compare.js';
+import { readFileSync } from 'fs';
+import { resolve, extname } from 'path';
+import parse from './parsers.js';
 import formatter from './formatters/index.js';
+import compare from './compare.js';
 
-const readFile = (filePath) => fs.readFileSync(path.resolve(process.cwd(), filePath), 'utf8');
-const getExt = (filePath) => path.extname(filePath);
+const getTypeFile = (filepath) => extname(filepath).slice(1);
+const getData = (filepath) => parse(readFileSync(filepath, 'utf-8'), getTypeFile(filepath));
+const buildFullPath = (filepath) => resolve(filepath);
 
-const genDiff = (filepath1, filepath2, format = 'stylish') => {
-  const file1 = readFile(filepath1);
-  const file2 = readFile(filepath2);
-  const ext1 = getExt(filepath1);
-  const ext2 = getExt(filepath2);
-
-  const obj1 = getParsedData(file1, ext1);
-  const obj2 = getParsedData(file2, ext2);
+const genDiff = (filePath1, filePath2, format = 'stylish') => {
+  const obj1 = getData(buildFullPath(filePath1));
+  const obj2 = getData(buildFullPath(filePath2));
   const tree = compare(obj1, obj2);
   return formatter(tree, format);
 };
