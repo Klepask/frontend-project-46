809b288381780f28e3b4b38f2bc0b7b0885f28d1
Eva Ulnitskaya <evaulnitsakaya@yandex.ru>
2024-01-05 23:15:02 +0300

    update index.js and parsers.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import fs from 'fs';
 import path from 'path';
-import getParsedData from './parsers.js';
+import getParsedData from './parsers.js'; 
 import compare from './compare.js';
 import formatter from './formatters/index.js';
 
-const readAndParseFile = (filePath) => {
-  const fileContent = fs.readFileSync(path.resolve(process.cwd(), filePath), 'utf8');
-  const fileExt = path.extname(filePath);
-  return getParsedData(fileContent, fileExt);
+const formats = {
+  '.json': 'json',
+  '.yml': 'yml', 
+  '.yaml': 'yaml'
 };
+
+const readAndParseFile = (filePath) => {
+  const fileContent = fs.readFileSync(filePath, 'utf8');
+  const fileExt = path.extname(filePath);
+  const format = formats[fileExt];
+  return getParsedData(fileContent, format);
+};
+
 
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const obj1 = readAndParseFile(filepath1);
