78d6a189df8f8e522e0c26c53fec18b6a233c294
Eva Ulnitskaya <evaulnitsakaya@yandex.ru>
2024-01-05 17:18:44 +0300

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import fs from 'fs';
 import path from 'path';
-import getParsedData from './parsers.js'; 
+import getParsedData from './parsers.js';
 import compare from './compare.js';
 import formatter from './formatters/index.js';
 
 const readFile = (filePath) => {
   const ext = getExt(filePath);
   const fileContent = fs.readFileSync(path.resolve(process.cwd(), filePath), 'utf8');
-  return getParsedData(fileContent, ext); 
+  return getParsedData(fileContent, ext);
 }
 
 const getExt = (filePath) => path.extname(filePath);
@@ -16,7 +16,7 @@ const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const obj1 = readFile(filepath1); 
   const obj2 = readFile(filepath2);
   const tree = compare(obj1, obj2);
-  return formatter(tree, format);  
+  return formatter(tree, format);
 };
 
 export default genDiff;
