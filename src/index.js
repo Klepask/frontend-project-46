7559c247b6ed50a9eadb1918280edb6d30ea01c0
Eva Ulnitskaya <evaulnitsakaya@yandex.ru>
2024-01-05 22:34:35 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 import fs from 'fs';
 import path from 'path';
-import getParsedData from './parsers.js';
+import getParsedData from './parsers.js'; 
 import compare from './compare.js';
 import formatter from './formatters/index.js';
 
-const readFile = (filePath) => fs.readFileSync(path.resolve(process.cwd(), filePath), 'utf8');
-const getExt = (filePath) => path.extname(filePath);
+const readAndParseFile = (filePath) => {
+  const fileContent = fs.readFileSync(path.resolve(process.cwd(), filePath), 'utf8');
+  const fileExt = path.extname(filePath);
+  return getParsedData(fileContent, fileExt);
+}
 
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
-  const file1 = readFile(filepath1);
-  const file2 = readFile(filepath2);
-  const ext1 = getExt(filepath1);
-  const ext2 = getExt(filepath2);
-
-  const obj1 = getParsedData(file1, ext1);
-  const obj2 = getParsedData(file2, ext2);
+  const obj1 = readAndParseFile(filepath1);
+  const obj2 = readAndParseFile(filepath2);
+  
   const tree = compare(obj1, obj2);
+  
   return formatter(tree, format);
-};
+}
 
 export default genDiff;
