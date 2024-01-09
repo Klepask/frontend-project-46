3a2e7014a7b087737f4751ebdf75ff719352ef5d
Eva Ulnitskaya <evaulnitsakaya@yandex.ru>
2024-01-09 22:43:32 +0300

    fix index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,12 +9,12 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename); // путь к фикстурам
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8'); // чтение фикстур
 
-const extensions = ['yml', 'json'];
+const formats = ['yml', 'json'];
 const expectedStylish = readFile('expected_stylish.txt');
 const expectedPlain = readFile('expected_plain.txt');
 const expectedJson = readFile('expected_json.txt');
 
-test.each(extensions)(`genDiff test ${extensions} format`, (ext) => {
+test.each(formats)('test %s format', (ext) => {
   const file1 = getFixturePath(`file1.${ext}`);
   const file2 = getFixturePath(`file2.${ext}`);
 
