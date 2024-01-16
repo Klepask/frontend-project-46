10361868278f6a2bf653c554e68e09d8d0b2cf60
Eva Ulnitskaya <evaulnitsakaya@yandex.ru>
2024-01-16 22:20:27 +0300

    update compare.js

--- src/compare.js
+++ src/compare.js
@@ -3,44 +3,56 @@ import _ from 'lodash';
 const compare = (data1, data2) => {
   const keys1 = Object.keys(data1);
   const keys2 = Object.keys(data2);
+  
   const sortKeys = _.sortBy(_.union(keys1, keys2));
 
   return sortKeys.map((key) => {
     const value1 = data1[key];
-    const value2 = data2[key];
+    
     if (!Object.hasOwn(data1, key)) {
-      return {
-        key,
-        value: value2,
-        type: 'added',
-      };
+      if (Object.hasOwn(data2, key)) {
+        return {
+          key,
+          value: data2[key],
+          type: 'added'
+        };
+      } else {
+        return {
+          key, 
+          type: 'added'
+        };
+      }
     }
+    
     if (!Object.hasOwn(data2, key)) {
       return {
         key,
         value: value1,
-        type: 'deleted',
+        type: 'deleted'
       };
     }
-    if (_.isObject(data1[key]) && _.isObject(data2[key])) {
+    
+    if (_.isObject(value1) && _.isObject(data2[key])) {
       return {
         type: 'nested',
         key,
-        children: compare(value1, value2),
+        children: compare(value1, data2[key])  
       };
     }
-    if (data1[key] !== data2[key]) {
+    
+    if (value1 !== data2[key]) {
       return {
         key,
-        value1,
-        value2,
-        type: 'changed',
+        value1, 
+        value2: data2[key],
+        type: 'changed'
       };
     }
+    
     return {
-      key,
+      key, 
       value: value1,
-      type: 'unchanged',
+      type: 'unchanged'
     };
   });
 };
