b67704975f5c465688d37fc647f8ab4dda3f876a
Eva Ulnitskaya <evaulnitsakaya@yandex.ru>
2024-01-16 23:18:03 +0300

    fix compare.js

--- src/compare.js
+++ src/compare.js
@@ -1,28 +1,30 @@
 import _ from 'lodash';
 
 const compare = (data1, data2) => {
+
   const keys1 = Object.keys(data1);
   const keys2 = Object.keys(data2);
-
+  
   const sortKeys = _.sortBy(_.union(keys1, keys2));
 
   return sortKeys.map((key) => {
+    
     const value1 = data1[key];
 
     if (!Object.hasOwn(data1, key)) {
       if (Object.hasOwn(data2, key)) {
-       return {
-         key,
-         value: data2[key],
-         type: 'added',
-        };
-      }else{
         return {
           key,
-          tupe: 'added'
+          value: data2[key],
+          type: 'added',
         };
       }
+      return {
+        key,
+        type: 'added',  
+      };
     }
+
     if (!Object.hasOwn(data2, key)) {
       return {
         key,
@@ -30,27 +32,31 @@ const compare = (data1, data2) => {
         type: 'deleted',
       };
     }
+
     if (_.isObject(data1[key]) && _.isObject(data2[key])) {
-      return {
+      return { 
         type: 'nested',
         key,
-        children: compare(value1, data2[key]),
+        children: compare(value1, data2[key]),  
       };
     }
+
     if (value1 !== data2[key]) {
       return {
-        key,
+        key, 
         value1,
-        value2: data2 [key],
+        value2: data2[key],  
         type: 'changed',
       };
     }
+
     return {
       key,
       value: value1,
       type: 'unchanged',
     };
   });
+
 };
 
 export default compare;
