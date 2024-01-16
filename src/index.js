ca2bfaae3dc5bc705e3b82d5fbc9db5b5a2497be
Eva Ulnitskaya <evaulnitsakaya@yandex.ru>
2024-01-16 22:09:27 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -3,16 +3,21 @@ import path from 'path';
 import getParsedData from './parsers.js';
 import compare from './compare.js';
 import formatter from './formatters/index.js';
+import process from 'process';
 
-const readAndParseFile = (filePath) => {
-  const fileContent = fs.readFileSync(filePath, 'utf8');
+const getFullPath = (filePath) => {
+  return path.resolve(process.cwd(), filePath);
+}
+const parseFile = (filePath) => {
+  const fullPath = getFullPath(filePath);
+  const fileContent = fs.readFileSync(fullPath, 'utf8');
   const fileExt = path.extname(filePath).slice(1);
   return getParsedData(fileContent, fileExt);
 };
 
 const gendiff = (filepath1, filepath2, format = 'stylish') => {
-  const file1Data = readAndParseFile(filepath1);
-  const file2Data = readAndParseFile(filepath2);
+  const file1Data = parseFile(filepath1);
+  const file2Data = parseFile(filepath2);
   const diff = compare(file1Data, file2Data);
   return formatter(diff, `.${format}`);
 };
