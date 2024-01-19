f2214e0eb62d69b46e97890ae1057d3cf917e1ca
Eva Ulnitskaya <evaulnitsakaya@yandex.ru>
2024-01-19 20:26:54 +0300

    update plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -7,28 +7,28 @@ const getValueType = (value) => {
   return typeof value === 'string' ? `'${value}'` : `${value}`;
 };
 
-const formatPlain = (tree) => {
-  const iter = (nodes, keyName = '') => {
-    const result = nodes.filter((node) => node.type !== 'unchanged')
-      .map((item) => {
-        const { type } = item;
-        const keyPath = [...keyName, item.key].join('');
-        switch (type) {
-          case 'deleted':
-            return `Property '${keyPath}' was removed`;
-          case 'changed':
-            return `Property '${keyPath}' was updated. From ${getValueType(item.value1)} to ${getValueType(item.value2)}`;
-          case 'added':
-            return `Property '${keyPath}' was added with value: ${getValueType(item.value)}`;
-          case 'nested':
-            return iter(item.children, `${keyPath}.`);
-          default:
-            throw new Error(`Unknown type: '${type}'!`);
-        }
-      });
-    return result.join('\n');
-  };
-  return iter(tree, '');
+
+const iter = (nodes, keyName = '') => {
+  const result = nodes.filter((node) => node.type !== 'unchanged')
+    .map((item) => {
+      const { type } = item;
+      const keyPath = [...keyName, item.key].join('');
+      switch (type) {
+        case 'deleted':
+          return `Property '${keyPath}' was removed`;
+        case 'changed':
+          return `Property '${keyPath}' was updated. From ${getValueType(item.value1)} to ${getValueType(item.value2)}`;
+        case 'added':
+          return `Property '${keyPath}' was added with value: ${getValueType(item.value)}`;
+        case 'nested':
+          return iter(item.children, `${keyPath}.`);
+        default:
+          throw new Error(`Unknown type: '${type}'!`);
+      }
+    });
+  return result.join('\n');
 };
+
+const formatPlain = (tree) => iter(tree, '');
 
 export default formatPlain;
