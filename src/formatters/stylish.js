d53ae66e7adf2e5f0ca78345ccac2aa4e2afbf26
Eva Ulnitskaya <evaulnitsakaya@yandex.ru>
2024-01-19 20:15:47 +0300

    update stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -18,32 +18,33 @@ const stringify = (node, depth) => {
   return `{\n${result.join('\n')}\n  ${indent(depth)}}`;
 };
 
-const formatStylish = (tree) => {
-  const iter = (node, depth) => {
-    if (typeof node !== 'object' || node === null) {
-      return `${node}`;
-    }
+const iter = (node, depth) => {
+  if (typeof node !== 'object' || node === null) {
+    return `${node}`;
+  }
 
-    const result = node.map((item) => {
-      const { type } = item;
-      switch (type) {
-        case 'deleted':
-          return `${indent(depth)}- ${item.key}: ${stringify(item.value, depth)}`;
-        case 'unchanged':
-          return `${indent(depth)}  ${item.key}: ${stringify(item.value, depth)}`;
-        case 'changed':
-          return (`${indent(depth)}- ${item.key}: ${stringify(item.value1, depth)}\n${indent(depth)}+ ${item.key}: ${stringify(item.value2, depth)}`);
-        case 'added':
-          return `${indent(depth)}+ ${item.key}: ${stringify(item.value, depth)}`;
-        case 'nested':
-          return `${indent(depth)}  ${item.key}: ${iter(item.children, depth + 1)}`;
-        default:
-          throw new Error(`Unknown type: '${type}'!`);
-      }
-    });
-    return `{\n${result.join('\n')}\n${indent(depth, 'last')}}`;
-  };
-  return iter(tree, 1);
+  const result = node.map((item) => {
+    const { type } = item;
+    switch (type) {
+      case 'deleted':
+        return `${indent(depth)}- ${item.key}: ${stringify(item.value, depth)}`;
+      case 'unchanged':
+        return `${indent(depth)}  ${item.key}: ${stringify(item.value, depth)}`;
+      case 'changed':
+        return (`${indent(depth)}- ${item.key}: ${stringify(item.value1, depth)}\n${indent(depth)}+ ${item.key}: ${stringify(item.value2, depth)}`);
+      case 'added':
+        return `${indent(depth)}+ ${item.key}: ${stringify(item.value, depth)}`;
+      case 'nested':
+        return `${indent(depth)}  ${item.key}: ${iter(item.children, depth + 1)}`;
+      default:
+        throw new Error(`Unknown type: '${type}'!`);
+    }
+  });
+  return `{\n${result.join('\n')}\n${indent(depth, 'last')}}`;
+};
+
+const formatStylish = (tree) => {
+  return iter(tree, 1)
 };
 
 export default formatStylish;
