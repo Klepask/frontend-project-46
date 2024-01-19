da25c8d0a090ca992e43892c840557673476e4d6
Eva Ulnitskaya <evaulnitsakaya@yandex.ru>
2024-01-19 21:36:45 +0300

    fix plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -9,7 +9,7 @@ const getValueType = (value) => {
   }
 };
 
-const getKeyPath = (keyName, key) => keyName ? `${keyName}.${key}` : key;
+const getKeyPath = (keyName, key) => (keyName ? `${keyName}.${key}` : key);
 
 const iter = (nodes, keyName = '') => {
   const result = nodes.filter((node) => node.type !== 'unchanged')
