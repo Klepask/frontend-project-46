c9418450f17043589562e4274cea90c24d7fb863
Eva Ulnitskaya <evaulnitsakaya@yandex.ru>
2024-02-12 19:58:18 +0300

    update compare.js

--- src/compare.js
+++ src/compare.js
@@ -4,9 +4,9 @@ const compare = (data1, data2) => {
   const keys1 = Object.keys(data1);
   const keys2 = Object.keys(data2);
 
-  const sortKeys = _.sortBy(_.union(keys1, keys2));
+  const sortedKeys = _.sortBy(_.union(keys1, keys2));
 
-  return sortKeys.map((key) => {
+  return sortedKeys.map((key) => {
     if (!Object.hasOwn(data1, key)) {
       return {
         key,
