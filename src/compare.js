43bf9fb59a30796a5fd5d43ca9c4975716fcc4a5
Eva Ulnitskaya <evaulnitsakaya@yandex.ru>
2024-02-12 19:14:37 +0300

    update compare.js

--- src/compare.js
+++ src/compare.js
@@ -7,7 +7,6 @@ const compare = (data1, data2) => {
   const sortKeys = _.sortBy(_.union(keys1, keys2));
 
   return sortKeys.map((key) => {
-    const value1 = data1[key];
     if (!Object.hasOwn(data1, key)) {
       return {
         key,
@@ -15,6 +14,8 @@ const compare = (data1, data2) => {
         type: 'added',
       };
     }
+
+    const value1 = data1[key]; // теперь определение value1 находится после проверки наличия ключа в data1
 
     if (!Object.hasOwn(data2, key)) {
       return {
@@ -24,19 +25,21 @@ const compare = (data1, data2) => {
       };
     }
 
-    if (_.isObject(data1[key]) && _.isObject(data2[key])) {
+    const value2 = data2[key]; // определяем value2 для дальнейшего использования.
+
+    if (_.isObject(value1) && _.isObject(value2)) {
       return {
         type: 'nested',
         key,
-        children: compare(value1, data2[key]),
+        children: compare(value1, value2),
       };
     }
 
-    if (value1 !== data2[key]) {
+    if (value1 !== value2) {
       return {
         key,
         value1,
-        value2: data2[key],
+        value2,
         type: 'changed',
       };
     }
